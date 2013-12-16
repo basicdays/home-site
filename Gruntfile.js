53e4bb29c7148b29dfb92e69b2de4642afe30aca
basicdays <paul@basicdays.com>
2013-12-16 10:03:52 -0600

    Rearrange some components. P2

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,41 +4,44 @@ var path = require('path');
 var clone = require('clone-component');
 
 exports = module.exports = function(grunt) {
+	//add lib folder to require path; add node_modules bin programs to shell path
 	var env = clone(process.env);
 	env.NODE_PATH = 'lib';
 	env.PATH = path.resolve('node_modules/.bin') + path.delimiter + env.PATH;
-
-	var shellOptions = {
-		stdout: true,
-		stderr: true,
-		execOptions: {
-			env: env
-		}
-	};
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		debugPort: 50010,
 
 		shell: {
-			componentInstall: {command: 'component install --dev', options: shellOptions},
-			componentBuild: {command: 'component build --out ./lib/webUI/public/build', options: shellOptions},
-			componentBuildDev: {command: 'component build --dev --out ./test/build', options: shellOptions},
-			jshint: {command: 'jshint *.js **/*.js', options: shellOptions},
-			mocha: {command: 'mocha --harmony-generators --reporter spec --timeout 15s', options: shellOptions},
-			server: {command: 'node --harmony-generators server.js', options: shellOptions},
-			testServer: {command: 'http-server test', options: shellOptions}
+			options: { stdout: true, stderr: true, execOptions: { env: env }},
+			componentInstall: {command: 'component install --dev'},
+			componentBuild: {command: 'component build --out ./lib/webUI/public/build'},
+			componentBuildTest: {command: 'component build --out ./test/build'},
+			jshint: {command: 'jshint *.js **/*.js'},
+			mocha: {command: 'mocha --harmony-generators --reporter spec --timeout 15s'},
+			server: {command: 'node --harmony-generators server.js'},
+			testServer: {command: 'http-server test'}
 		},
 
-		clean: ['components', 'lib/webUI/public/build', 'test/build']
+		clean: ['components', 'lib/webUI/public/build', 'test/build'],
+
+		watch: {
+			options: { spawn: false, interval: 500 },
+			components: {
+				files: ['lib/webUI/components/**/*', 'test/components/**/*'],
+				tasks: ['shell:componentBuild', 'shell:componentBuildTest']
+			}
+		}
 	});
 
 	grunt.loadNpmTasks('grunt-shell');
 	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	grunt.registerTask('default', ['server']);
 	grunt.registerTask('install', ['shell:componentInstall']);
-	grunt.registerTask('build', ['shell:componentBuild', 'shell:componentBuildDev']);
+	grunt.registerTask('build', ['shell:componentBuild', 'shell:componentBuildTest']);
 	grunt.registerTask('test', ['shell:jshint', 'shell:mocha']);
 	grunt.registerTask('server', ['shell:server']);
 	grunt.registerTask('testServer', ['shell:testServer']);
